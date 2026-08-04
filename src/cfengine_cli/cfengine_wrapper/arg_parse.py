7ca735388818f96a4879c32f3f8fa0bd72dfec78
Simon Halvorsen <simon.halvorsen@northern.tech>
2026-08-04 14:01:17 +0200

    Bumped cf-remote version & changed how parse/validation of `cf-remote` functions is done
    Ticket: ENT-14305
    Changelog: Title
    
    Signed-off-by: Simon Halvorsen <simon.halvorsen@northern.tech>

--- src/cfengine_cli/cfengine_wrapper/arg_parse.py
+++ src/cfengine_cli/cfengine_wrapper/arg_parse.py
@@ -1,27 +1,21 @@
 import argparse
 
+from cf_remote.args import (
+    add_save_args,
+    add_deploy_args,
+    add_install_args,
+    add_uninstall_args,
+    add_spawn_args,
+    add_destroy_args,
+)
+
 
 def parse_wrapper_args(subp: argparse._SubParsersAction):
 
-    sp = subp.add_parser(
-        "save", help="Save host(s) with a group name to use in other commands"
-    )
-    sp.add_argument(
-        "--role",
-        help="Role of the hosts",
-        choices=["hub", "hubs", "client", "clients"],
-        required=True,
-    )
-    sp.add_argument(
-        "--name",
-        help="Name of the group of hosts (can be used in other commands)",
-        required=True,
-    )
-    sp.add_argument(
-        "--hosts",
-        "-H",
-        help="SSH usernames and IPs for SSH and CFEngine in the form of user@ip",
-        required=True,
+    add_save_args(
+        subp.add_parser(
+            "save", help="Save host(s) with a group name to use in other commands"
+        )
     )
 
     sp = subp.add_parser(
@@ -40,18 +34,13 @@ def parse_wrapper_args(subp: argparse._SubParsersAction):
         help="""Build a policy set from a CFEngine Build project.
 A wrapper around the cfbs `build`-function.""",
     )
-    sp = subp.add_parser(
+
+    deploy_parser = subp.add_parser(
         "deploy",
         help="""Deploy policy-set (masterfiles) to hub.
 A wrapper around the cf-remote `deploy`-function with some added niceties.""",
     )
-    sp.add_argument("--hub", help="Hub(s) to deploy to", type=str)
-    sp.add_argument(
-        "masterfiles",
-        help="Policy-set location (tarball URL or local path to tarball / directory)",
-        type=str,
-        nargs="?",
-    )
+    add_deploy_args(deploy_parser)
 
     install_parser = subp.add_parser(
         "install",
@@ -64,78 +53,14 @@ A wrapper around the cf-remote `deploy`-function with some added niceties.""",
         help="Specify version",
         type=str,
     )
-    # install_parser._option_string_actions.get("--version").help = "absdfsf"
-    # TODO: Update cf-remote/cfbs to have more modular arg-parsing, then we can import
-    # and override any differences? technically illegal since _option_string_actions,
-    # but will save ~ 200-1000 loc depending on how much we import into cfengine-cli
-
-    install_parser.add_argument(
-        "--edition",
-        "-E",
-        choices=["community", "enterprise"],
-        help="Enterprise or community packages",
-        type=str,
-    )
-    install_parser.add_argument(
-        "--package", help="Local path to package or URL to download", type=str
-    )
-    install_parser.add_argument(
-        "--hub-package",
-        help="Local path to package or URL to download for --hub",
-        type=str,
-    )
-    install_parser.add_argument(
-        "--client-package",
-        help="Local path to package or URL to download for --clients",
-        type=str,
-    )
-    install_parser.add_argument(
-        "--bootstrap", "-B", help="cf-agent --bootstrap argument", type=str
-    )
-    install_parser.add_argument(
-        "--clients", "-c", help="Where to install client package", type=str
-    )
-    install_parser.add_argument("--hub", help="Where to install hub package", type=str)
-    install_parser.add_argument(
-        "--demo",
-        help="Use defaults to make demos smoother (NOT secure)",
-        action="store_true",
-    )
-    install_parser.add_argument(
-        "--call-collect",
-        help="Enable call collect in --demo def.json",
-        action="store_true",
-    )
-    install_parser.add_argument(
-        "--remote-download",
-        help="Package will be downloaded directly to the target machine",
-        action="store_true",
-    )
-    install_parser.add_argument(
-        "--trust-keys",
-        help="Comma-separated list of paths to keys hosts should trust"
-        + " (implies '--trust-server no' when boostraping)",
-        type=str,
-    )
-    install_parser.add_argument(
-        "--insecure",
-        help="Ignore mismatching checksums when downloading urls",
-        action="store_true",
-    )
+    add_install_args(install_parser)
 
     uninstall_parser = subp.add_parser(
         "uninstall",
         help="Uninstall CFEngine on the given hosts",
         description="A wrapper around the cf-remote `uninstall` function",
     )
-    uninstall_parser.add_argument(
-        "--purge", help="Complete uninstallation", action="store_true"
-    )
-    uninstall_parser.add_argument(
-        "--clients", "-c", help="Where to uninstall", type=str
-    )
-    uninstall_parser.add_argument("--hub", help="Where to uninstall", type=str)
-    uninstall_parser.add_argument("--hosts", "-H", help="Where to uninstall", type=str)
+    add_uninstall_args(uninstall_parser)
 
     report_parser = subp.add_parser(
         "report",
@@ -184,73 +109,19 @@ A wrapper around the cf-remote `run`-function with some added niceties",
         "If omitted and multiple installations are found, you'll be prompted.",
     )
 
-    sp = subp.add_parser(
+    spawn_parser = subp.add_parser(
         "spawn",
         help="Spawn hosts in the clouds",
         description="A wrapper around the cf-remote `spawn`-function",
     )
-    sp.add_argument(
-        "--list-platforms", help="List supported platforms", action="store_true"
-    )
-    sp.add_argument(
-        "--list-boxes", help="List installed vagrant boxes", action="store_true"
-    )
-    sp.add_argument(
-        "--init-config",
-        help="Initialize configuration file for spawn functionality",
-        action="store_true",
-    )
-    sp.add_argument("--platform", help="Platform or vagrant box to use", type=str)
-    sp.add_argument("--count", default=1, help="How many hosts to spawn", type=int)
-    sp.add_argument(
-        "--role", help="Role of the hosts", choices=["hub", "hubs", "client", "clients"]
-    )
-    sp.add_argument(
-        "--name", help="Name of the group of hosts (can be used in other commands)"
-    )
-    sp.add_argument(
-        "--append",
-        help="Append the new VMs to a pre-existing group",
-        action="store_true",
-    )
-    sp.add_argument(
-        "--provider",
-        help="VM provider",
-        type=str,
-        default="aws",
-        choices=["aws", "gcp", "vagrant"],
-    )
-    sp.add_argument("--cpus", help="Number of CPUs of the vagrant instances", type=int)
-    sp.add_argument(
-        "--sync-folder",
-        help="Root folder of synchronized folders of vagrant instance",
-        type=str,
-    )
-    sp.add_argument(
-        "--provision",
-        help="full path to provision shell script for Vagrant VM",
-        type=str,
-    )
-    sp.add_argument("--size", help="Size/type of the instances", type=str)
-    sp.add_argument(
-        "--network", help="network/subnet to assign the VMs to (GCP only)", type=str
-    )
-    sp.add_argument(
-        "--no-public-ip",
-        help="No public IP needed (GCP only; WARNING: The VMs will only be accessible"
-        + " from some other VM in the same cloud/network!)",
-        action="store_true",
-    )
+    add_spawn_args(spawn_parser)
 
-    dp = subp.add_parser(
+    destroy_parser = subp.add_parser(
         "destroy",
         help="Destroy hosts spawned in the clouds",
         description="A wrapper around the cf-remote `destroy`-function",
     )
-    dp.add_argument(
-        "--all", help="Destroy all hosts spawned in the clouds", action="store_true"
-    )
-    dp.add_argument("name", help="Name of the group of hosts to destroy", nargs="?")
+    add_destroy_args(destroy_parser)
 
     profile_parser = subp.add_parser(
         "profile", help="Parse CFEngine profiling output (cf-agent -Kp)"
